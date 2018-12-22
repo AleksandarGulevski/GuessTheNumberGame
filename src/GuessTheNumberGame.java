c5159019dd6d9a72487442e6703384b6d7ef236a
Aleksandar Gulevski <agulevski10@gmail.com>
2018-12-22 16:47:12 +0100

    Validating input.

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -1,50 +1,64 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumberGame {
 
-       public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            int anyRandomNumber = (int) (Math.random() * 100) + 1;
-            boolean isWinner = false;
+        int anyRandomNumber = (int) (Math.random() * 100) + 1;
+        boolean isWinner = false;
 
-           System.out.print("I`m thinking of a number between 1 and 100 (Including both).\n" +
-                   " Can you guess what it is?\n" +
-                   " Type a number: ");
+        System.out.print("I`m thinking of a number between 1 and 100 (Including both).\n" +
+                " Can you guess what it is?\n" +
+                " Type a number: ");
 
-           Scanner in = new Scanner (System.in);
-           for (int i = 10; i >0; i--) {
-               System.out.println("You have " + i + " guess(es) left. Take a guess: ");
-               int yourGuess = in.nextInt();
-
-               if (anyRandomNumber > yourGuess) {
-                   System.out.println("Your guess is to low");
+        Scanner in = new Scanner(System.in);
 
 
-               } else if (anyRandomNumber < yourGuess) {
-                   System.out.println("Your guess is to high");
+        for (int i = 9; i >= 0; i--) {
+            int yourGuess = 1;
 
+            boolean isNumeric = false;//This will be set to true when numeric val entered
+            while (!isNumeric)
+                try {
 
-               } else {
-                 isWinner = true;
-                 break;
+                    int guess = in.nextInt();
+                    in.nextLine();
+                    isNumeric = true;//numeric value entered, so break the while loop
 
+                    yourGuess = guess;
+                } catch (InputMismatchException ime) {
+                    //Display Error message
+                    System.out.println("Invalid character found," +
+                            " Please enter numeric values only !!");
+                    in.nextLine();//Advance the scanner
+                }
 
-               }
-           }
+            System.out.println("You have " + i + " guess(es) left. Take a guess: ");
 
-           if (isWinner) {
-               System.out.println("You guess it right. You are the champion!");
+            if (yourGuess < 1 || yourGuess > 100) {
+                System.out.println("That is not a valid number. Please enter a number between 1 and 100 (Including both).");
+            } else {
+                if (anyRandomNumber > yourGuess) {
+                    System.out.println("Your guess is to low");
+                } else if (anyRandomNumber < yourGuess) {
+                    System.out.println("Your guess is to high");
+                } else {
+                    isWinner = true;
+                    break;
+                }
+            }
+        }
 
-           } else {
-               System.out.println("Game Over. You did not guess the number i was thinking ");
-               System.out.println("The number i was thinking was " + anyRandomNumber);
-           }
-       }
+        if (isWinner) {
+            System.out.println("You guess it right. You are the champion!");
 
-
+        } else {
+            System.out.println("Game Over. You did not guess the number i was thinking " +
+                    "\nThe number i was thinking was " + anyRandomNumber);
+        }
+    }
 
 
 }
-
-
